a537c661989519ab9e38ab8edd80930506a3ddbc
pmeier <me@patrick-meier.io>
2021-01-05 18:09:04 +0100

    Added hello-world.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, DoCheck, ViewChild} from '@angular/core';
+import {HelloWorldComponent} from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements DoCheck{
+
+  @ViewChild('helloWorldComponent') hwComp: HelloWorldComponent;
+
+  label = {name: 'Daniel'};
+
+  displayLabel() {
+    alert('Label = ' + this.hwComp.test);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+
 }
